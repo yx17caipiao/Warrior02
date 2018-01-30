3ebc2c700ae392312d6c566899b849414369f9f7
yx17caipiao <yx_17caipiao@163.com>
2018-01-30 23:06:05 +0800

    ~

--- src/main/java/com/warrior/netease/splash/activity/WebViewActivity.java
+++ src/main/java/com/warrior/netease/splash/activity/WebViewActivity.java
@@ -3,7 +3,12 @@ package com.warrior.netease.splash.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+import com.warrior.netease.R;
 import com.warrior.netease.splash.bean.Action;
 
 /**
@@ -12,10 +17,34 @@ import com.warrior.netease.splash.bean.Action;
 
 public class WebViewActivity extends Activity {
     public static final String ACTION_NAME = "action";
+    WebView wb_new;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Action action = (Action)getIntent().getSerializableExtra(ACTION_NAME);
+        setContentView(R.layout.activity_webview);
+        wb_new = (WebView)findViewById(R.id.wb_new);
+        wb_new.getSettings().setJavaScriptEnabled(true);
+        wb_new.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        wb_new.getSettings().setUseWideViewPort(true);
+        wb_new.getSettings().setLoadWithOverviewMode(true);
+
+//        wb_new.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+//        wb_new.getSettings().setSupportMultipleWindows(true);
+        wb_new.setWebViewClient(new WebViewClient());
+//        wb_new.setWebChromeClient(new WebChromeClient());
+
+
+        wb_new.loadUrl(action.getLink_url());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(wb_new.canGoBack()){
+            wb_new.goBack();
+            return;
+        }
+        super.onBackPressed();
     }
 }
