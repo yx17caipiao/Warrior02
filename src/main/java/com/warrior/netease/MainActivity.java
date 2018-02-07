849a2a381e14897da1cdecb7c199d130ed0eafb3
yx17caipiao <yx_17caipiao@163.com>
2018-02-07 16:26:04 +0800

    -

--- src/main/java/com/warrior/netease/MainActivity.java
+++ src/main/java/com/warrior/netease/MainActivity.java
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TabHost;
+import android.widget.TextView;
 
 import com.warrior.netease.news.fragment.EmptyFragment;
 import com.warrior.netease.news.fragment.NewsFragment;
@@ -18,18 +20,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FragmentTabHost tabHost = (FragmentTabHost) findViewById(R.id.tab_Host);
+
+        String[] titles = getResources().getStringArray(R.array.tab_title);
+        int [] icons = new int[]{R.drawable.news_selector,R.drawable.reading_selector,R.drawable.video_selector,R.drawable.topic_selector,R.drawable.mine_selector};
+        Class[] classes = new Class[]{NewsFragment.class,EmptyFragment.class,EmptyFragment.class,EmptyFragment.class,EmptyFragment.class};
         tabHost.setup(this,getSupportFragmentManager(),R.id.content);
 //
-        TabHost.TabSpec one = tabHost.newTabSpec("1");
-        one.setIndicator(getEveryView(this));
-        TabHost.TabSpec two = tabHost.newTabSpec("2");
-        two.setIndicator(getEveryView(this));
-        TabHost.TabSpec three = tabHost.newTabSpec("3");
-        three.setIndicator(getEveryView(this));
+        for(int i=0;i<titles.length;i++){
+            TabHost.TabSpec tmp =  tabHost.newTabSpec(""+i);
+            tmp.setIndicator(getEveryView(this,titles,icons,i));
+            tabHost.addTab(tmp,classes[i],null);
+        }
 
-        tabHost.addTab(one, NewsFragment.class,null);
-        tabHost.addTab(two, EmptyFragment.class,null);
-        tabHost.addTab(three, EmptyFragment.class,null);
+
 
         tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
             @Override
@@ -37,13 +40,18 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+//        tabHost.setCurrentTabByTag("2");
 
     }
 
 
-    public View getEveryView(Context context){
+    public View getEveryView(Context context,String[] titles,int[] icons,int index){
         LayoutInflater inflater = LayoutInflater.from(context);
         View title_view = inflater.inflate(R.layout.item_title,null);
+        TextView title = (TextView) title_view.findViewById(R.id.title);
+        ImageView icon = (ImageView) title_view.findViewById(R.id.icon);
+        title.setText(titles[index]);
+        icon.setImageResource(icons[index]);
         return title_view;
     }
 }
